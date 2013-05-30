c70cdcd5ac611ac8e4796ed6e4e2cc0d5d8e946d
Yuzhi Wang <johnwyz88@gmail.com>
2013-05-30 17:56:33 -0400

    Fix cam

--- Assets/Scripts/MoonCarControl.js
+++ Assets/Scripts/MoonCarControl.js
@@ -1,4 +1,9 @@
 var avatar0 : Transform;
+var Player : GameObject;
+var playerClone: String;
+
 function OnLoaded(){ 
  	Network.Instantiate(avatar0, transform.position, transform.rotation, 0);
+ 	playerClone = "EmptyMoonCar(Clone)";  
+  	Player = GameObject.Find(playerClone);
 }
--- Assets/Standard Assets (Mobile)/Scripts/car_control.js
+++ Assets/Standard Assets (Mobile)/Scripts/car_control.js
@@ -5,7 +5,7 @@ var style:GUIStyle;
 function OnGUI () {
     
     if(Application.platform == RuntimePlatform.Android){
-    	
+    Debug.Log("Running in Android");
 	style.fontStyle = FontStyle.Italic;
 	style.normal.textColor = Color.white;
 	style.fontSize = 40;
@@ -74,8 +74,77 @@ function OnGUI () {
 	}
 	
 }
-    else if(Application.platform == RuntimePlatform.WindowsPlayer){
-    	
+    else if(Application.platform == RuntimePlatform.WindowsPlayer || Application.platform == RuntimePlatform.WindowsEditor){
+    Debug.Log("Running in PC");
+    style.fontStyle = FontStyle.Italic;
+	style.normal.textColor = Color.white;
+	style.fontSize = 40;
+	GUI.Label(Rect(Screen.width - 370, 80, 100, 100),"Speed: ", style);
+	style.fontSize = 110;
+	style.fontStyle = FontStyle.BoldAndItalic;
+	if (Input.touches.Length == 0) {
+		
+		style.normal.textColor = Color.white;
+		GUI.Label(Rect(Screen.width - 220, 40, 100, 100),"X 0", style);
+		if(Input.acceleration.x < -0.05){
+			transform.Rotate(0,Input.acceleration.x * turnSpeed,0); 
+		} else if(Input.acceleration.x > 0.05){
+			transform.Rotate(0,Input.acceleration.x * turnSpeed,0); 
+		}
+		rigidbody.AddRelativeForce(0,-Physics.gravity.magnitude * 4000,0);
+		
+			
+	} else if(Input.touches.Length == 1){
+		
+		style.normal.textColor = Color.green;
+		GUI.Label(Rect(Screen.width - 220, 40, 100, 100),"X 1", style);
+		rigidbody.AddRelativeForce(forwardSpeed,0,0);
+		if(Input.acceleration.x < -0.05){
+			transform.Rotate(0,Input.acceleration.x * (turnSpeed-1),0); 
+		} else if(Input.acceleration.x > 0.05){
+			transform.Rotate(0,Input.acceleration.x * (turnSpeed-1),0); 
+		}
+		rigidbody.AddRelativeForce(0,-Physics.gravity.magnitude * 4000,0);
+	
+	} else if(Input.touches.Length == 2){
+		
+		style.normal.textColor = Color.blue;
+		GUI.Label(Rect(Screen.width - 220, 40, 100, 100),"X 2", style);
+		rigidbody.AddRelativeForce(forwardSpeed * 2,0,0);
+		if(Input.acceleration.x < -0.05){
+			transform.Rotate(0,Input.acceleration.x * (turnSpeed-2),0); 
+		} else if(Input.acceleration.x > 0.05){
+			transform.Rotate(0,Input.acceleration.x * (turnSpeed-2),0); 
+		}
+		rigidbody.AddRelativeForce(0,-Physics.gravity.magnitude * 8000,0);
+		
+	} else if(Input.touches.Length == 3){
+		
+		style.normal.textColor = Color.yellow;
+		GUI.Label(Rect(Screen.width - 220, 40, 100, 100),"X 3", style);
+		rigidbody.AddRelativeForce(forwardSpeed * 3,0,0);
+		if(Input.acceleration.x < -0.05){
+			transform.Rotate(0,Input.acceleration.x * (turnSpeed-3),0); 
+		} else if(Input.acceleration.x > 0.05){
+			transform.Rotate(0,Input.acceleration.x * (turnSpeed-3),0); 
+		}
+		rigidbody.AddRelativeForce(0,-Physics.gravity.magnitude * 16000,0);
+		
+	} else if(Input.touches.Length > 3){
+		
+		style.normal.textColor = Color.magenta;
+		GUI.Label(Rect(Screen.width - 220, 40, 100, 100),"X 4", style);
+		rigidbody.AddRelativeForce(forwardSpeed * 4,0,0);
+		if(Input.acceleration.x < -0.05){
+			transform.Rotate(0,Input.acceleration.x * (turnSpeed-4),0); 
+		} else if(Input.acceleration.x > 0.05){
+			transform.Rotate(0,Input.acceleration.x * (turnSpeed-4),0); 
+		}
+		rigidbody.AddRelativeForce(0,-Physics.gravity.magnitude * 24000,0);
+	}
     }
-    else{}
+    else{
+    	Debug.Log("Running in others");
+    	Debug.Log(Application.platform.ToString());
+    }
 }
