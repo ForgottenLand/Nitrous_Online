4998f62df5b2f802cced905d981fff6de3bd89fd
Yuzhi Wang <johnwyz88@gmail.com>
2013-06-05 22:02:05 -0400

    Started on authentication js

---
+++ Assets/Scripts/Authentication.js
@@ -0,0 +1,39 @@
+	import System;
+	import System.Data;
+	import System.Data.SqlClient;
+	var dbcmd : IDbCommand;
+	var cmdSql : String;
+	
+	function Start () {
+    	
+	var dbcon : IDbConnection;
+    var connectionString : String =
+        "Server=96.31.33.42;" + // put the ip here!
+        "Database=SQL2012_924853_forgotten;" +
+        "User ID=SQL2012_924853_forgotten_user;" +
+        "Password=Passnew1994!;";
+    dbcon = new SqlConnection(connectionString);
+    dbcon.Open();
+    
+    //create the class EXECUTIONER, that execute SQL commands
+    dbcmd = dbcon.CreateCommand();
+    //string var, to save the command we want to use
+    cmdSql = "SELECT UserName, Email, Password FROM [SQL2012_924853_forgotten].[dbo].[Table1]";
+    //we add the command, as string, to the executor, to shot it!
+    dbcmd.CommandText = cmdSql;
+    //and then, we create a table, like a normal db table, to use it on unity, and we use the function that "plays" the command
+    var reader : IDataReader = dbcmd.ExecuteReader();
+    
+    while(reader .Read()) {
+        var UserName : String = reader ["UserName"].ToString();
+        var Email : String = reader ["Email"].ToString();
+        var Password : String = reader ["Password"].ToString();
+        print ("UserName: " + UserName + "Email: " + Email + "Password: " + Password);
+    }
+        
+    reader .Close();
+    reader = null;
+    dbcon.Close();
+    dbcon = null;
+    
+    }
