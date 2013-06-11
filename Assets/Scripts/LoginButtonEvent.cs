984018f27d765129aef740d77325540b58ed7ba8
Yuzhi Wang <johnwyz88@gmail.com>
2013-06-10 20:46:27 -0400

    message log

--- Assets/Scripts/LoginButtonEvent.cs
+++ Assets/Scripts/LoginButtonEvent.cs
@@ -3,18 +3,29 @@ using System.Collections;
 
 public class LoginButtonEvent : MonoBehaviour {
 	
-	public GameObject UsernameObj, PasswordObj, authObj;
+	public GameObject usernameObj, passwordObj, authObj, messageObj;
 	
 	void OnClick ()
 	{
 		Authentication authScript = (Authentication) authObj.GetComponent(typeof(Authentication));
-		UILabel username = (UILabel) UsernameObj.GetComponent(typeof(UILabel));
-		UILabel password = (UILabel) PasswordObj.GetComponent(typeof(UILabel));
+		UILabel username = (UILabel) usernameObj.GetComponentInChildren(typeof(UILabel));
+		UILabel password = (UILabel) passwordObj.GetComponentInChildren(typeof(UILabel));
+		UILabel message = (UILabel) messageObj.GetComponent(typeof(UILabel));
 		if(authScript.Authenticate(username.text,password.text)){
 			Application.LoadLevel("NGUIPreRace");
 		} else {
-			
+			message.text = "Sorry, your username or password is incorrect.";
+			username.text = "";
+			password.text = "";
 		}
+	}
+	
+	void OnAwake()
+	{
+		DontDestroyOnLoad(usernameObj);
+		DontDestroyOnLoad(passwordObj);
+		DontDestroyOnLoad(authObj);
+		DontDestroyOnLoad(messageObj);	
 	}
 	
 }
