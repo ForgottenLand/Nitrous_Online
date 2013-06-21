438fabe887fee9df11197b0a81bb7399bff898c7
Yuzhi Wang <ywang@kobo.com>
2013-06-21 13:57:03 -0400

    Attempting to fix multiplayer unsync issue

--- Assets/Scripts/AdminSpawnControl.js
+++ Assets/Scripts/AdminSpawnControl.js
@@ -16,21 +16,16 @@ public var selectNumber : int;
 var selected : boolean;
 
 //The Player GameObject and its name
-public static var Player : GameObject;
+var Player : GameObject;
 var playerClone: String;
-
 var style:GUIStyle;
+
 
 //Luke Variables---------------
 var frLeft : WheelCollider;
 var frRight : WheelCollider; 
 var reLeft : WheelCollider; 
 var reRight : WheelCollider;
-
-var speed: float;
-var impulse: float;
-
-var LCar : LukeCar;
 
 var enginePower :float;
 var maxSteer : float;
@@ -41,8 +36,6 @@ var steer : float;
 var speedTurn: float;
 var speed : float;
 var brakePower: float;
-
-
 
 var drifting = false;
 var driftingTimer : float;
@@ -67,24 +60,35 @@ var steerMultiplier : float;
 
 var maxSpeed : float;
 var minDriftSpeed : float;
-var style : GUIStyle;
 //-----------------------------
 
 
-
-
-
-
 function OnLoaded() {
-     btnX = Screen.width * 0.01;
-     btnY = Screen.width * 0.01;
-     btnW = Screen.width * 0.2;
-     btnH = Screen.width * 0.05;
+    btnX = Screen.width * 0.01;
+    btnY = Screen.width * 0.01;
+    btnW = Screen.width * 0.1;
+    btnH = Screen.width * 0.05;
      //reverse = false;
-     
-     enginePower = 10;
-     maxSteer = 12;
-     
+	
+	enginePower = 15;
+    maxSteer = 12;
+    power = 0;
+    brake = 0;
+    steer = 0;
+    speedTurn = 30;
+    speed = 0;
+    brakePower = 50;
+    driftingTimeout = 0.15;
+    regFriction = 0.3;
+    driftFriction = 0.03;
+    driftFwdFriction = 0.5;
+    frictionRatio = 1.25;
+    startDriftAngVel = 0.6;
+    endDriftAngVel = 0.3;
+    rigidbodyAngVel = 0;
+    steerMultiplier = 1.75;
+    maxSpeed = 280;
+    minDriftSpeed = 130;
 }
 
 function SpawnCar(){ 
@@ -92,54 +96,69 @@ function SpawnCar(){
     switch(selectNumber){
         case 0:
             Network.Instantiate(avatar0, transform.position, transform.rotation, 0);
-            playerClone = avatar0.name + "(Clone)"; 
+            playerClone = avatar0.name+"(Clone)"; 
+            
         break;
         case 1:
             Network.Instantiate(avatar1, transform.position, transform.rotation, 0);
-            playerClone = avatar1.name + "(Clone)"; 
+            playerClone = avatar1.name+"(Clone)"; 
+
         break;
         case 2:
             Network.Instantiate(avatar2, transform.position, transform.rotation, 0);
-            playerClone = avatar2.name + "(Clone)"; 
+            playerClone = avatar2.name+"(Clone)"; 
+
         break;
         case 3:
             Network.Instantiate(avatar3, transform.position, transform.rotation, 0);
-            playerClone = avatar3.name + "(Clone)"; 
+            playerClone = avatar3.name+"(Clone)";
+
         break;
         case 4:
             Network.Instantiate(avatar4, transform.position, transform.rotation, 0);
-            playerClone = avatar4.name + "(Clone)";  
+            playerClone = avatar4.name+"(Clone)"; 
+
         break;
     }
  	
   	Player = GameObject.Find(playerClone);
-    //Player.rigidbody.freezeRotation = true;
+    Player.rigidbody.centerOfMass=Vector3(0,-0.9,0.3);
+    pos = Player.transform.position;
+    prevPos = pos;
+    
+    frLeft = Player.Find("FrontLeft").GetComponent(WheelCollider);
+    frRight = Player.Find("FrontRight").GetComponent(WheelCollider);
+    reLeft = Player.Find("RearLeft").GetComponent(WheelCollider);
+    reRight = Player.Find("RearRight").GetComponent(WheelCollider);
+    
+    Debug.Log(Player.ToString());
+    Debug.Log(frLeft);
 }
 
 function OnGUI(){
-     
+    
      if(!selected){
-         if(GUI.Button(Rect(btnX, btnY * 3, btnW, btnH), "MoonCar")){
+         if(GUI.Button(Rect(btnX, btnY * 3, btnW, btnH), avatar0.name)){
              selectNumber = 0;
              selected = true;
              SpawnCar();
          }
-         else if(GUI.Button(Rect(btnX, btnY * 9, btnW, btnH), "Arcade")){
+         else if(GUI.Button(Rect(btnX, btnY * 9, btnW, btnH), avatar1.name)){
              selectNumber = 1;
              selected = true;
              SpawnCar();
          }
-         else if(GUI.Button(Rect(btnX, btnY * 15, btnW, btnH), "Charger")){
+         else if(GUI.Button(Rect(btnX, btnY * 15, btnW, btnH), avatar2.name)){
              selectNumber = 2;
              selected = true;
              SpawnCar();
          }
-         else if(GUI.Button(Rect(btnX, btnY * 21, btnW, btnH), "Colt")){
+         else if(GUI.Button(Rect(btnX, btnY * 21, btnW, btnH), avatar3.name)){
              selectNumber = 3;
              selected = true;
              SpawnCar();
          }
-         else if(GUI.Button(Rect(btnX, btnY * 27, btnW, btnH), "Pgt")){
+         else if(GUI.Button(Rect(btnX, btnY * 27, btnW, btnH), avatar4.name)){
              selectNumber = 4;
              selected = true;
              SpawnCar();
@@ -152,142 +171,146 @@ function OnGUI(){
     
 
 function Update()
-{       
-//    style.fontStyle = FontStyle.Italic;
-//    style.normal.textColor = Color.white;
-//    style.fontSize = 40;
-//    GUI.Label(Rect(Screen.width - 370, 80, 100, 100),"Speed: ", style);
-//    style.fontSize = 40;
-//    style.fontStyle = FontStyle.BoldAndItalic;
-//    style.normal.textColor = Color.white;
-	
-	speed=Player.rigidbody.velocity.magnitude * 3.6;
-
-    power=Input.GetAxis("Vertical") * enginePower * Time.deltaTime * 250.0;
-    steer=Input.GetAxis("Horizontal") * maxSteer * Mathf.Clamp(speedTurn/speed, 0, 1);
-    brake=Input.GetButton("Jump") ? brakePower: 0.0;   
-	
-	
-	rigidbodyAngVel = Player.rigidbody.angularVelocity.y;
-	
-	if((rigidbodyAngVel >= startDriftAngVel || rigidbodyAngVel <= - startDriftAngVel) && speed >= minDriftSpeed)
-	{
-		driftingTimer += Time.deltaTime;
-	}
-	if(driftingTimer >= driftingTimeout)
-	{
-		startDrift = true;
-		driftingTimer = 0;
-	}
-	
-	if(rigidbodyAngVel > 0)
-	{
-		if(rigidbodyAngVel <= endDriftAngVel || speed <= minDriftSpeed/3)
+{
+	try{
+//		style.fontStyle = FontStyle.Italic;
+//	    style.normal.textColor = Color.white;
+//	    style.fontSize = 40;
+//	    GUI.Label(Rect(Screen.width - 370, 80, 100, 100),"Speed: ", style);
+//	    style.fontSize = 40;
+//	    style.fontStyle = FontStyle.BoldAndItalic;
+//	    style.normal.textColor = Color.white;
+		
+		speed=Player.rigidbody.velocity.magnitude * 3.6;
+		
+	    power=Input.GetAxis("Vertical") * enginePower * Time.deltaTime * 250.0;
+	    steer=Input.GetAxis("Horizontal") * maxSteer * Mathf.Clamp(speedTurn/speed, 0, 1);
+	    brake=Input.GetButton("Jump") ? brakePower: 0.0;   
+		
+		
+		rigidbodyAngVel = Player.rigidbody.angularVelocity.y;
+		
+		if((rigidbodyAngVel >= startDriftAngVel || rigidbodyAngVel <= - startDriftAngVel) && speed >= minDriftSpeed)
 		{
-			endDrift = true;
+			driftingTimer += Time.deltaTime;
 		}
-	}
-	else
-	{
-		if(rigidbodyAngVel >= -endDriftAngVel || speed <= minDriftSpeed/3)
+		if(driftingTimer >= driftingTimeout)
 		{
-			endDrift = true;
+			startDrift = true;
+			driftingTimer = 0;
 		}
-	}
-
-	//determine this based on forward speed and (angularVelocity?) or another method of determining sideways force.
-	if(startDrift)
-	{
-		drifting = true;
-		startDrift = false;
-	}
-	else if(endDrift)
-	{
-		drifting = false;
-		endDrift = false;
-		driftingTimer = 0;
-	}
-
-    
-    if(speed < maxSpeed)
-    {
-        frLeft.brakeTorque=power/10;
-		frRight.brakeTorque=power/10;
-        reLeft.brakeTorque=power/10;
-        reRight.brakeTorque=power/10;
-        reLeft.motorTorque=power;
-        reRight.motorTorque=power;
-    }
-    else
-    {
-    	frLeft.brakeTorque=power/10;
-		frRight.brakeTorque=power/10;
-        reLeft.brakeTorque=power/10;
-        reRight.brakeTorque=power/10;
-        reLeft.motorTorque=0;
-        reRight.motorTorque=0;
-    }
-    
-    if(drifting)
-    {
-    	if(rigidbodyAngVel > 0)
-    	{
-    		frLeft.sidewaysFriction.stiffness = Mathf.Clamp(Mathf.Pow(Mathf.Lerp(driftFriction, regFriction, rigidbodyAngVel/endDriftAngVel), 2), driftFriction, regFriction);
-        	frRight.sidewaysFriction.stiffness = Mathf.Clamp(Mathf.Pow(Mathf.Lerp(driftFriction, regFriction, rigidbodyAngVel/endDriftAngVel), 2), driftFriction, regFriction);
-        	reLeft.sidewaysFriction.stiffness = Mathf.Clamp(Mathf.Pow(Mathf.Lerp(driftFriction/frictionRatio, regFriction, rigidbodyAngVel/endDriftAngVel), 2), driftFriction/frictionRatio, regFriction);
-        	reRight.sidewaysFriction.stiffness = Mathf.Clamp(Mathf.Pow(Mathf.Lerp(driftFriction/frictionRatio, regFriction, rigidbodyAngVel/endDriftAngVel), 2), driftFriction/frictionRatio, regFriction);
-        }
-        else
-        {
-       		frLeft.sidewaysFriction.stiffness = Mathf.Clamp(Mathf.Pow(Mathf.Lerp(driftFriction, regFriction, -rigidbodyAngVel/endDriftAngVel), 2), driftFriction, regFriction);
-        	frRight.sidewaysFriction.stiffness = Mathf.Clamp(Mathf.Pow(Mathf.Lerp(driftFriction, regFriction, -rigidbodyAngVel/endDriftAngVel), 2), driftFriction, regFriction);
-        	reLeft.sidewaysFriction.stiffness = Mathf.Clamp(Mathf.Pow(Mathf.Lerp(driftFriction/frictionRatio, regFriction, -rigidbodyAngVel/endDriftAngVel), 2), driftFriction/frictionRatio, regFriction);
-        	reRight.sidewaysFriction.stiffness = Mathf.Clamp(Mathf.Pow(Mathf.Lerp(driftFriction/frictionRatio, regFriction, -rigidbodyAngVel/endDriftAngVel), 2), driftFriction/frictionRatio, regFriction);
-        }
-        
-        frLeft.forwardFriction.stiffness = Mathf.Clamp(Mathf.Pow(Mathf.Lerp(driftFwdFriction, 1, rigidbodyAngVel/endDriftAngVel), 2), driftFwdFriction, 1);
-        frRight.forwardFriction.stiffness = Mathf.Clamp(Mathf.Pow(Mathf.Lerp(driftFwdFriction, 1, rigidbodyAngVel/endDriftAngVel), 2), driftFwdFriction, 1);
-        reLeft.forwardFriction.stiffness = Mathf.Clamp(Mathf.Pow(Mathf.Lerp(driftFwdFriction, 1, rigidbodyAngVel/endDriftAngVel), 2), driftFwdFriction, 1);
-        reRight.forwardFriction.stiffness = Mathf.Clamp(Mathf.Pow(Mathf.Lerp(driftFwdFriction, 1, rigidbodyAngVel/endDriftAngVel), 2), driftFwdFriction, 1);
-    	
-    	//driftingTimer += Time.deltaTime;
-    	
-    	steer *= steerMultiplier;
-    	
-    }
-    else
-    {
-    	frLeft.sidewaysFriction.stiffness = regFriction;
-        frRight.sidewaysFriction.stiffness = regFriction;
-        reLeft.sidewaysFriction.stiffness = regFriction;
-        reRight.sidewaysFriction.stiffness = regFriction;
-        
-        frLeft.forwardFriction.stiffness = 1;
-        frRight.forwardFriction.stiffness = 1;
-        reLeft.forwardFriction.stiffness = 1;
-        reRight.forwardFriction.stiffness = 1;
-    }
-    
-//    if(driftingTimer >= driftingTimeout)
-//    {
-//    	drifting = false;
-//    	driftingTimer = 0.0f;
-//    }
-    
-    if(brake > 0.0)
-    {
-        frLeft.brakeTorque=50;
-        frRight.brakeTorque=50;
-        reLeft.brakeTorque=50;
-        reRight.brakeTorque=50;
-
-        reLeft.motorTorque=0.0;
-        reRight.motorTorque=0.0;
-    } 
-    
-    
-    frLeft.steerAngle=steer;
-    frRight.steerAngle=steer;
-    
-    GUI.Label(Rect(Screen.width - 220, 80, 100, 100), speed.ToString(), style);
+		
+		if(rigidbodyAngVel > 0)
+		{
+			if(rigidbodyAngVel <= endDriftAngVel || speed <= minDriftSpeed/3)
+			{
+				endDrift = true;
+			}
+		}
+		else
+		{
+			if(rigidbodyAngVel >= -endDriftAngVel || speed <= minDriftSpeed/3)
+			{
+				endDrift = true;
+			}
+		}
+	
+		//determine this based on forward speed and (angularVelocity?) or another method of determining sideways force.
+		if(startDrift)
+		{
+			drifting = true;
+			startDrift = false;
+		}
+		else if(endDrift)
+		{
+			drifting = false;
+			endDrift = false;
+			driftingTimer = 0;
+		}
+	
+	    
+	    if(speed < maxSpeed)
+	    {
+	        frLeft.brakeTorque=power/10;
+			frRight.brakeTorque=power/10;
+	        reLeft.brakeTorque=power/10;
+	        reRight.brakeTorque=power/10;
+	        reLeft.motorTorque=power;
+	        reRight.motorTorque=power;
+	    }
+	    else
+	    {
+	    	frLeft.brakeTorque=power/10;
+			frRight.brakeTorque=power/10;
+	        reLeft.brakeTorque=power/10;
+	        reRight.brakeTorque=power/10;
+	        reLeft.motorTorque=0;
+	        reRight.motorTorque=0;
+	    }
+	    
+	    if(drifting)
+	    {
+	    	if(rigidbodyAngVel > 0)
+	    	{
+	    		frLeft.sidewaysFriction.stiffness = Mathf.Clamp(Mathf.Pow(Mathf.Lerp(driftFriction, regFriction, rigidbodyAngVel/endDriftAngVel), 2), driftFriction, regFriction);
+	        	frRight.sidewaysFriction.stiffness = Mathf.Clamp(Mathf.Pow(Mathf.Lerp(driftFriction, regFriction, rigidbodyAngVel/endDriftAngVel), 2), driftFriction, regFriction);
+	        	reLeft.sidewaysFriction.stiffness = Mathf.Clamp(Mathf.Pow(Mathf.Lerp(driftFriction/frictionRatio, regFriction, rigidbodyAngVel/endDriftAngVel), 2), driftFriction/frictionRatio, regFriction);
+	        	reRight.sidewaysFriction.stiffness = Mathf.Clamp(Mathf.Pow(Mathf.Lerp(driftFriction/frictionRatio, regFriction, rigidbodyAngVel/endDriftAngVel), 2), driftFriction/frictionRatio, regFriction);
+	        }
+	        else
+	        {
+	       		frLeft.sidewaysFriction.stiffness = Mathf.Clamp(Mathf.Pow(Mathf.Lerp(driftFriction, regFriction, -rigidbodyAngVel/endDriftAngVel), 2), driftFriction, regFriction);
+	        	frRight.sidewaysFriction.stiffness = Mathf.Clamp(Mathf.Pow(Mathf.Lerp(driftFriction, regFriction, -rigidbodyAngVel/endDriftAngVel), 2), driftFriction, regFriction);
+	        	reLeft.sidewaysFriction.stiffness = Mathf.Clamp(Mathf.Pow(Mathf.Lerp(driftFriction/frictionRatio, regFriction, -rigidbodyAngVel/endDriftAngVel), 2), driftFriction/frictionRatio, regFriction);
+	        	reRight.sidewaysFriction.stiffness = Mathf.Clamp(Mathf.Pow(Mathf.Lerp(driftFriction/frictionRatio, regFriction, -rigidbodyAngVel/endDriftAngVel), 2), driftFriction/frictionRatio, regFriction);
+	        }
+	        
+	        frLeft.forwardFriction.stiffness = Mathf.Clamp(Mathf.Pow(Mathf.Lerp(driftFwdFriction, 1, rigidbodyAngVel/endDriftAngVel), 2), driftFwdFriction, 1);
+	        frRight.forwardFriction.stiffness = Mathf.Clamp(Mathf.Pow(Mathf.Lerp(driftFwdFriction, 1, rigidbodyAngVel/endDriftAngVel), 2), driftFwdFriction, 1);
+	        reLeft.forwardFriction.stiffness = Mathf.Clamp(Mathf.Pow(Mathf.Lerp(driftFwdFriction, 1, rigidbodyAngVel/endDriftAngVel), 2), driftFwdFriction, 1);
+	        reRight.forwardFriction.stiffness = Mathf.Clamp(Mathf.Pow(Mathf.Lerp(driftFwdFriction, 1, rigidbodyAngVel/endDriftAngVel), 2), driftFwdFriction, 1);
+	    	
+	    	//driftingTimer += Time.deltaTime;
+	    	
+	    	steer *= steerMultiplier;
+	    	
+	    }
+	    else
+	    {
+	    	frLeft.sidewaysFriction.stiffness = regFriction;
+	        frRight.sidewaysFriction.stiffness = regFriction;
+	        reLeft.sidewaysFriction.stiffness = regFriction;
+	        reRight.sidewaysFriction.stiffness = regFriction;
+	        
+	        frLeft.forwardFriction.stiffness = 1;
+	        frRight.forwardFriction.stiffness = 1;
+	        reLeft.forwardFriction.stiffness = 1;
+	        reRight.forwardFriction.stiffness = 1;
+	    }
+	    
+	//    if(driftingTimer >= driftingTimeout)
+	//    {
+	//    	drifting = false;
+	//    	driftingTimer = 0.0f;
+	//    }
+	    
+	    if(brake > 0.0)
+	    {
+	        frLeft.brakeTorque=50;
+	        frRight.brakeTorque=50;
+	        reLeft.brakeTorque=50;
+	        reRight.brakeTorque=50;
+	
+	        reLeft.motorTorque=0.0;
+	        reRight.motorTorque=0.0;
+	    } 
+	    
+	    
+	    frLeft.steerAngle=steer;
+	    frRight.steerAngle=steer;
+	    
+	    GUI.Label(Rect(Screen.width - 220, 80, 100, 100), speed.ToString(), style);
+	    }
+	catch(UnityException)
+	{}
 }
--- Assets/Scripts/LukeCar.js
+++ Assets/Scripts/LukeCar.js
@@ -1,35 +1,46 @@
 //CarController1.js
 
-var fl : WheelCollider;
-var fr : WheelCollider; 
-var rl : WheelCollider; 
-var rr : WheelCollider;
+var frLeft : WheelCollider;
+var frRight : WheelCollider; 
+var reLeft : WheelCollider; 
+var reRight : WheelCollider;
 
-var enginePower=150.0;
-var maxSteer=25.0;
+var enginePower :float;
+var maxSteer : float;
 
-var power=0.0;
-var brake=0.0;
-var steer=0.0;
+var power : float;
+var brake : float;
+var steer : float;
 var speedTurn: float;
-
 var speed : float;
+var brakePower: float;
 
-var style:GUIStyle;
+
 
 var drifting = false;
-var driftingTimer = 0.0f;
-var driftingTimeout = 2.0f;
+var driftingTimer : float;
+var driftingTimeout : float;
 
-var regFriction: float;
-var driftFriction: float;
-var driftFwdFriction: float;
-var frictionRatio: float;
+var regFriction : float;
+var driftFriction : float;
+var driftFwdFriction : float;
+var frictionRatio : float;
 
 var startDrift;
 var endDrift;
 var pos;
 var prevPos;
+
+var startDriftAngVel : float;
+var endDriftAngVel : float;
+
+var rigidbodyAngVel : float;
+
+var steerMultiplier : float;
+
+var maxSpeed : float;
+var minDriftSpeed : float;
+var style : GUIStyle;
 
 function Awake(){
     
@@ -40,6 +51,8 @@ function Awake(){
 }
 
 function Update () {
+
+	
 	style.fontStyle = FontStyle.Italic;
     style.normal.textColor = Color.white;
     style.fontSize = 40;
@@ -48,76 +61,113 @@ function Update () {
     style.fontStyle = FontStyle.BoldAndItalic;
     style.normal.textColor = Color.white;
 	
-	speed=rigidbody.velocity.magnitude*3.6;
+	speed=rigidbody.velocity.magnitude * 3.6;
+	
+	
 	
     power=Input.GetAxis("Vertical") * enginePower * Time.deltaTime * 250.0;
     steer=Input.GetAxis("Horizontal") * maxSteer * Mathf.Clamp(speedTurn/speed, 0, 1);
-    brake=Input.GetButton("Jump") ? rigidbody.mass * 0.1: 0.0;   
+    brake=Input.GetButton("Jump") ? brakePower: 0.0;   
+	
+	
+	rigidbodyAngVel = this.rigidbody.angularVelocity.y;
+	
+	if((rigidbodyAngVel >= startDriftAngVel || rigidbodyAngVel <= - startDriftAngVel) && speed >= minDriftSpeed)
+	{
+		driftingTimer += Time.deltaTime;
+	}
+	if(driftingTimer >= driftingTimeout)
+	{
+		startDrift = true;
+		driftingTimer = 0;
+	}
+	
+	if(rigidbodyAngVel > 0)
+	{
+		if(rigidbodyAngVel <= endDriftAngVel || speed <= minDriftSpeed/3)
+		{
+			endDrift = true;
+		}
+	}
+	else
+	{
+		if(rigidbodyAngVel >= -endDriftAngVel || speed <= minDriftSpeed/3)
+		{
+			endDrift = true;
+		}
+	}
 
+	//determine this based on forward speed and (angularVelocity?) or another method of determining sideways force.
 	if(startDrift)
 	{
 		drifting = true;
+		startDrift = false;
 	}
 	else if(endDrift)
 	{
 		drifting = false;
+		endDrift = false;
+		driftingTimer = 0;
 	}
 
-    if(brake > 0.0){
-
-        fl.brakeTorque=brake;
-        fr.brakeTorque=brake;
-        rl.brakeTorque=brake;
-        rr.brakeTorque=brake;
-
-        rl.motorTorque=0.0;
-        rr.motorTorque=0.0;
-        
-        //drifting = true;
-        //driftingTimer = 0;
-        
-        
-        
-        //steer *= 3;
-    } else {
-
-        fl.brakeTorque=0;
-		fr.brakeTorque=0;
-        rl.brakeTorque=0;
-        rr.brakeTorque=0;
-        rl.motorTorque=power;
-        rr.motorTorque=power;
+    
+    if(speed < maxSpeed)
+    {
+        frLeft.brakeTorque=power/10;
+		frRight.brakeTorque=power/10;
+        reLeft.brakeTorque=power/10;
+        reRight.brakeTorque=power/10;
+        reLeft.motorTorque=power;
+        reRight.motorTorque=power;
+    }
+    else
+    {
+    	frLeft.brakeTorque=power/10;
+		frRight.brakeTorque=power/10;
+        reLeft.brakeTorque=power/10;
+        reRight.brakeTorque=power/10;
+        reLeft.motorTorque=0;
+        reRight.motorTorque=0;
     }
     
     if(drifting)
     {
-    
-    	fl.sidewaysFriction.stiffness = Mathf.Clamp(Mathf.Pow(Mathf.Lerp(driftFriction, regFriction, driftingTimer/driftingTimeout), 2), driftFriction, 1);
-        fr.sidewaysFriction.stiffness = Mathf.Clamp(Mathf.Pow(Mathf.Lerp(driftFriction, regFriction, driftingTimer/driftingTimeout), 2), driftFriction, 1);
-        rl.sidewaysFriction.stiffness = Mathf.Clamp(Mathf.Pow(Mathf.Lerp(driftFriction/frictionRatio, regFriction, driftingTimer/driftingTimeout), 2), driftFriction/frictionRatio, 1);
-        rr.sidewaysFriction.stiffness = Mathf.Clamp(Mathf.Pow(Mathf.Lerp(driftFriction/frictionRatio, regFriction, driftingTimer/driftingTimeout), 2), driftFriction/frictionRatio, 1);
+    	if(rigidbodyAngVel > 0)
+    	{
+    		frLeft.sidewaysFriction.stiffness = Mathf.Clamp(Mathf.Pow(Mathf.Lerp(driftFriction, regFriction, rigidbodyAngVel/endDriftAngVel), 2), driftFriction, regFriction);
+        	frRight.sidewaysFriction.stiffness = Mathf.Clamp(Mathf.Pow(Mathf.Lerp(driftFriction, regFriction, rigidbodyAngVel/endDriftAngVel), 2), driftFriction, regFriction);
+        	reLeft.sidewaysFriction.stiffness = Mathf.Clamp(Mathf.Pow(Mathf.Lerp(driftFriction/frictionRatio, regFriction, rigidbodyAngVel/endDriftAngVel), 2), driftFriction/frictionRatio, regFriction);
+        	reRight.sidewaysFriction.stiffness = Mathf.Clamp(Mathf.Pow(Mathf.Lerp(driftFriction/frictionRatio, regFriction, rigidbodyAngVel/endDriftAngVel), 2), driftFriction/frictionRatio, regFriction);
+        }
+        else
+        {
+       		frLeft.sidewaysFriction.stiffness = Mathf.Clamp(Mathf.Pow(Mathf.Lerp(driftFriction, regFriction, -rigidbodyAngVel/endDriftAngVel), 2), driftFriction, regFriction);
+        	frRight.sidewaysFriction.stiffness = Mathf.Clamp(Mathf.Pow(Mathf.Lerp(driftFriction, regFriction, -rigidbodyAngVel/endDriftAngVel), 2), driftFriction, regFriction);
+        	reLeft.sidewaysFriction.stiffness = Mathf.Clamp(Mathf.Pow(Mathf.Lerp(driftFriction/frictionRatio, regFriction, -rigidbodyAngVel/endDriftAngVel), 2), driftFriction/frictionRatio, regFriction);
+        	reRight.sidewaysFriction.stiffness = Mathf.Clamp(Mathf.Pow(Mathf.Lerp(driftFriction/frictionRatio, regFriction, -rigidbodyAngVel/endDriftAngVel), 2), driftFriction/frictionRatio, regFriction);
+        }
         
-        fl.forwardFriction.stiffness = Mathf.Clamp(Mathf.Pow(Mathf.Lerp(driftFwdFriction, 1, driftingTimer/driftingTimeout), 2), driftFwdFriction, 1);
-        fr.forwardFriction.stiffness = Mathf.Clamp(Mathf.Pow(Mathf.Lerp(driftFwdFriction, 1, driftingTimer/driftingTimeout), 2), driftFwdFriction, 1);
-        rl.forwardFriction.stiffness = Mathf.Clamp(Mathf.Pow(Mathf.Lerp(driftFwdFriction, 1, driftingTimer/driftingTimeout), 2), driftFwdFriction, 1);
-        rr.forwardFriction.stiffness = Mathf.Clamp(Mathf.Pow(Mathf.Lerp(driftFwdFriction, 1, driftingTimer/driftingTimeout), 2), driftFwdFriction, 1);
+        frLeft.forwardFriction.stiffness = Mathf.Clamp(Mathf.Pow(Mathf.Lerp(driftFwdFriction, 1, rigidbodyAngVel/endDriftAngVel), 2), driftFwdFriction, 1);
+        frRight.forwardFriction.stiffness = Mathf.Clamp(Mathf.Pow(Mathf.Lerp(driftFwdFriction, 1, rigidbodyAngVel/endDriftAngVel), 2), driftFwdFriction, 1);
+        reLeft.forwardFriction.stiffness = Mathf.Clamp(Mathf.Pow(Mathf.Lerp(driftFwdFriction, 1, rigidbodyAngVel/endDriftAngVel), 2), driftFwdFriction, 1);
+        reRight.forwardFriction.stiffness = Mathf.Clamp(Mathf.Pow(Mathf.Lerp(driftFwdFriction, 1, rigidbodyAngVel/endDriftAngVel), 2), driftFwdFriction, 1);
     	
     	//driftingTimer += Time.deltaTime;
     	
-    	steer *= 3;
+    	steer *= steerMultiplier;
     	
     }
     else
     {
-    	fl.sidewaysFriction.stiffness = regFriction;
-        fr.sidewaysFriction.stiffness = regFriction;
-        rl.sidewaysFriction.stiffness = regFriction;
-        rr.sidewaysFriction.stiffness = regFriction;
+    	frLeft.sidewaysFriction.stiffness = regFriction;
+        frRight.sidewaysFriction.stiffness = regFriction;
+        reLeft.sidewaysFriction.stiffness = regFriction;
+        reRight.sidewaysFriction.stiffness = regFriction;
         
-        fl.forwardFriction.stiffness = 1;
-        fr.forwardFriction.stiffness = 1;
-        rl.forwardFriction.stiffness = 1;
-        rr.forwardFriction.stiffness = 1;
+        frLeft.forwardFriction.stiffness = 1;
+        frRight.forwardFriction.stiffness = 1;
+        reLeft.forwardFriction.stiffness = 1;
+        reRight.forwardFriction.stiffness = 1;
     }
     
 //    if(driftingTimer >= driftingTimeout)
@@ -126,9 +176,22 @@ function Update () {
 //    	driftingTimer = 0.0f;
 //    }
     
-    fl.steerAngle=steer;
-    fr.steerAngle=steer;
+    if(brake > 0.0)
+    {
+        frLeft.brakeTorque=50;
+        frRight.brakeTorque=50;
+        reLeft.brakeTorque=50;
+        reRight.brakeTorque=50;
+
+        reLeft.motorTorque=0.0;
+        reRight.motorTorque=0.0;
+    } 
+    
+    
+    frLeft.steerAngle=steer;
+    frRight.steerAngle=steer;
     
     GUI.Label(Rect(Screen.width - 220, 80, 100, 100), speed.ToString(), style);
+    
     
 }
